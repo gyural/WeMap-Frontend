dbe26ca528842a1c9673b2515fe09b44e6008ee5
seonnnn <jjinjuu317@gmail.com>
2023-09-14 13:58:34 +0900

    button update

--- we-map-app/src/components/accounts/HomeScreen.jsx
+++ we-map-app/src/components/accounts/HomeScreen.jsx
@@ -4,13 +4,18 @@ import React from 'react';
 import styled from 'styled-components';
 import img1 from "../../images/homemap.png";
 import colors from '../../Common/Color';
+
 const Container = styled.div`
   text-align: center;
+  width: 100%;
+  height: 100%;
+  padding-top: 10%; 
 `;
 
 const BigTitle = styled.h1`
   color: ${colors.black}; 
-  margin-top: 100px;
+  margin-top: 10%; 
+  font-size: 200%; 
   & span1 {
     color: ${colors.mainBlue}; 
   }
@@ -18,30 +23,33 @@ const BigTitle = styled.h1`
 
 const SmallTitle = styled.h3`
   color: ${colors.black}; 
+  font-size: 140%; 
   & span2 {
     color: ${colors.mainBlue}; 
   }
 `;
+
 const Image = styled.img`
-  width: 300px;
-  height: 300px; 
-  margin-top: 20px; 
+  width: 80%;
+  margin-top: 5%; 
 `;
+
 const Buttons = styled.div`
-  margin-top: 20px; 
+  margin-top: 5%; 
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
+
 const Button1 = styled.button`
   background-color: ${colors.mainBlue}; 
   color: ${colors.white};
-  padding: 10px 20px;
-  font-size: 25px;
+  padding: 1% 3%; 
+  font-size: 150%; 
   font-weight: bold;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  width: 25%;
+  margin-top: 2%; 
+  margin-bottom: 2%; 
+  width: 100%;
   border: none;
   cursor: pointer;
   border-radius: 20px;
@@ -53,11 +61,11 @@ const Button1 = styled.button`
 const Button2 = styled.button`
   background-color: ${colors.gray}; 
   color: ${colors.white};
-  padding: 10px 20px;
-  font-size: 25px;
+  padding: 1% 3%; 
+  font-size: 150%; 
   font-weight: bold;
-  margin-top: 10px;
-  width: 25%;
+  margin-top: 2%; 
+  width: 100%;
   border: none;
   cursor: pointer;
   border-radius: 20px;
@@ -67,8 +75,7 @@ const Button2 = styled.button`
 `;
 
 function HomeScreen(props) {
-
-  const moveAccounts = props.moveAccounts
+  const moveAccounts = props.moveAccounts;
   return (
     <Container>
       <BigTitle>Welcome to <span1>WeMap</span1> !</BigTitle>
@@ -76,11 +83,8 @@ function HomeScreen(props) {
       <Image src={img1} alt="WeMap 이미지" />
       <Buttons>
         <Button1>Get Started</Button1>
-        <Button2
-          onClick = {moveAccounts}
-        >Log In</Button2>
+        <Button2 onClick={moveAccounts}>Log In</Button2>
       </Buttons>
-
     </Container>
   );
 }
