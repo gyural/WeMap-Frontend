3d1f46f2311d65e2b69476dc59c1018daa75295a
gyural <dla93190@naver.com>
2023-09-18 16:16:52 +0900

    map Container 구조 수정중...

--- we-map-app/src/components/views/Landing/Sections/MapContainer.js
+++ we-map-app/src/components/views/Landing/Sections/MapContainer.js
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 import { createPolygon, getPolygonPath } from './createPolygon';
 import colors from '../../../../Common/Color';
 import { drawPolygon } from './createPolygon';
+import { insertManualCard } from './manualCard';
 import pinicon from '../../../../images/pin.png';
 // import FindLoad from './FindLoad';
 
 
+/**Map Container를 감싸는 최종 부모 컴포넌트 */
 const Container = styled.div`
     width: 100%;
     height: 100%;
@@ -20,15 +22,22 @@ const dummyLocations = [
   { title: "Location 3", lat: 37.289198, lng: 127.012131 },
   { title: "이재모피자", lat: 35.102102, lng: 129.030605 },
 ];
-
-
 const { kakao } = window;
 
 const MapContainer = ({ searchPlace }) => {
 
     const [locations, setLocations] = useState([]);
+
+    //socket에 대이터가 바뀔때마다 socketListener가 바뀜
     const [socketListenr, setSocketListenr] = useState([])
-  
+    
+    //zoom이 바뀔때마다 리랜더링을 통해서 보이게할 지도 컨텐츠를 조절하기 위함
+    const [zoom, setZoom] = useState(undefined)
+    
+    /**
+     * MapCotainer가 마운트 / 언마운트 될때만 작동한는 Hook
+     * 1) 웹소캣의 함수들의 정의 2) 웹소캣 connect / disconnect를 다룸
+     */
     useEffect(() => {
       // WebSocket 연결 생성
       const websocket = new WebSocket("wss://lvb2z5ix97.execute-api.ap-northeast-2.amazonaws.com/dev?token=sometoken");
@@ -54,21 +63,43 @@ const MapContainer = ({ searchPlace }) => {
       };
       // 컴포넌트 언마운트 시 연결 종료
       return () => {
+        console.log('언마운트 후 disconnected')
         websocket.close();
       };
     }, []);
 
+
     useEffect(() => {
-      
-    
+
+      //카카오 맵 객체 만들어 주기
       const mapContainer = document.getElementById('map');
       const mapOption = {
-          center: new kakao.maps.LatLng(36.498649, 127.268141),
+          center: new kakao.maps.LatLng(37.541, 126.986),
           level: 7
       };
-  
       const map = new kakao.maps.Map(mapContainer, mapOption);
-  
+      
+      //소캣으로 들어온 데이터로 map 객체에 다각형지도 Drawing
+      console.log(socketListenr)
+      if(socketListenr){
+        const sd_list = []
+        socketListenr.forEach(element => {
+          sd_list.push(Number(element.location_code))
+        });
+        drawPolygon(map, sd_list)
+      }
+
+      //zomm이 바뀔때 마다 메뉴얼카드 추가 / 삭제
+      kakao.maps.event.addListener(map, 'zoom_changed', function() {
+        var level = map.getLevel();
+        console.log('zoom Changed')
+        console.log(level)
+        if (level < 9){
+          insertManualCard(map)
+        }
+        setZoom(level)
+      })
+      
       const imageSrc =pinicon;
       const imageSize = new kakao.maps.Size(50, 50);
       const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
@@ -107,21 +138,7 @@ const MapContainer = ({ searchPlace }) => {
         customOverlay.setMap(null);
     });
   
-      /**
-     * 다각형지도 Drawing
-     */
-    console.log(socketListenr)
-    if(socketListenr){
-      const sd_list = []
-      socketListenr.forEach(element => {
-        sd_list.push(Number(element.location_id))
-      });
-      drawPolygon(map, sd_list)
-
-    }
-  
-  }, [searchPlace, locations]);
-  
+  }, [searchPlace, locations, socketListenr]);
   
 
     return (
