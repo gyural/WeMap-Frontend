a2110d1d2af3aa5c8a0d96ff2aa50554b946f8a2
seonnnn <jjinjuu317@gmail.com>
2023-09-23 12:40:25 +0900

    location_search

--- we-map-app/src/components/views/Landing/Sections/LocationSelector.jsx
+++ we-map-app/src/components/views/Landing/Sections/LocationSelector.jsx
@@ -17,7 +17,7 @@ const Button = styled.button`
 
 const StyledButton = styled.button`
   margin: 5px;
-  padding: 8px 15px;
+  padding: 20px 15px;
   column-gap: 20px;
 
   border: none;
@@ -44,13 +44,12 @@ const ModalOverlay = styled.div`
 
 const ModalContainer = styled.div`
   position: fixed;
-  top: 50%;
+  top: 40%;
   left: 50%;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
   min-height: 2dvh;
   transform: translate(-50%, -50%);
-  padding: 20px;
+  padding: 50px;
   width: 20%; 
   height: 50%; 
   border-radius: 12px;
@@ -58,10 +57,9 @@ const ModalContainer = styled.div`
   border: 1px solid #ddd;
   z-index: 1000;
   overflow-y: auto; 
-  ::-webkit-scrollbar {
+  &::-webkit-scrollbar {
     display: none;
   }
-  
 `;
 
 const LocationSelector = ({ onLocationSelect }) => {
@@ -70,76 +68,87 @@ const LocationSelector = ({ onLocationSelect }) => {
   const [selectedGugun, setSelectedGugun] = useState(null);
   const [locationData, setLocationData] = useState({});
 
-  const handleClick = async () => {
+  const fetchSidoData = async () => {
     try {
-      const response = await axios.get('https://q59cs7kvf3.execute-api.ap-northeast-2.amazonaws.com/plz/getCode', {
+      const response = await axios.get('https://f4e1b1y4g9.execute-api.ap-northeast-2.amazonaws.com/plz/sido');
+      const parsedData = JSON.parse(response.data.body); // body 속성 파싱
+      setLocationData({ sido: parsedData });
+    } catch (error) {
+      console.error("Error fetching sido data", error);
+    }
+  };
+
+  const fetchLocationData = async (url, data) => {
+    try {
+      const response = await axios.post(url, data, {
         headers: {
           'Content-Type': 'application/json',
         },
         withCredentials: true
       });
-      console.log(response)
-      // API 응답 데이터를 locationData 상태에 저장
-      setLocationData(response.data.body);
-      
-  
-    } catch (error) {
-      console.error("Error sending POST request", error);
+      const parsedData = JSON.parse(response.data.body); 
+      return parsedData;
+      } catch (error) {
+      console.error("Error fetching location data", error);
     }
-  
-    setIsOpen(!isOpen);
   };
-  
- 
 
-  
-
-  const handleSidoSelect = (sido) => {
+  const handleSidoSelect = async (sido) => {
     setSelectedSido(sido);
-    setSelectedGugun(null);
+    
+    const sidoValue = locationData.sido[sido]; 
+    const gugunData = await fetchLocationData('https://f4e1b1y4g9.execute-api.ap-northeast-2.amazonaws.com/plz/resion', { code: sidoValue.code });
+    setLocationData(prevState => ({ ...prevState, gugun: gugunData }));
   };
-
-  const handleGugunSelect = (gugun) => {
+  
+  const handleGugunSelect = async (gugun) => {
     setSelectedGugun(gugun);
+    const gugunValue = locationData.gugun[gugun]; 
+    const dongData = await fetchLocationData('https://f4e1b1y4g9.execute-api.ap-northeast-2.amazonaws.com/plz/resion', { code: locationData.sido[selectedSido], gugun: gugunValue.code });
+    setLocationData(prevState => ({ ...prevState, dong: dongData }));
   };
-
-  const handleLocationFinalSelect = (dong) => {
-    onLocationSelect(`${selectedSido} ${selectedGugun} ${dong}`);
+  
+  const handleDongSelect = async (dong) => {
+    const dongValue = locationData.dong[dong]; 
+    const fullLocation = `${locationData.sido[selectedSido]} ${locationData.gugun[selectedGugun]} ${dongValue}`;
+    const locationInfo = await fetchLocationData('https://f4e1b1y4g9.execute-api.ap-northeast-2.amazonaws.com/plz/resion', { code: fullLocation.code });
+    onLocationSelect(locationInfo);
     setIsOpen(false);
   };
+  
+  
+
+  useEffect(() => {
+    fetchSidoData();
+  }, []);
+
 
   return (
     <>
-      <Button onClick={handleClick}>지역 선택</Button>
-        
+      <Button onClick={() => setIsOpen(!isOpen)}>지역 선택</Button>
       <ModalOverlay open={isOpen}>
         <ModalContainer>
-          {!selectedSido && Object.keys(locationData).map(sido => (
+          {!selectedSido && Object.keys(locationData.sido || {}).map(sido => (
             <StyledButton key={sido} onClick={() => handleSidoSelect(sido)}>
               {sido}
             </StyledButton>
           ))}
-
-          {selectedSido && !selectedGugun && Object.keys(locationData[selectedSido]?.군구 || {}).map(gugun => (
+          {selectedSido && !selectedGugun && Object.keys(locationData.gugun || {}).map(gugun => (
             <StyledButton key={gugun} onClick={() => handleGugunSelect(gugun)}>
               {gugun}
             </StyledButton>
           ))}
-
-          {selectedSido && selectedGugun && Object.keys(locationData[selectedSido]?.군구[selectedGugun]?.읍면동 || {}).map(dong => (
-            <StyledButton key={dong} onClick={() => handleLocationFinalSelect(dong)}>
+          {selectedSido && selectedGugun && Object.keys(locationData.dong || {}).map(dong => (
+            <StyledButton key={dong} onClick={() => handleDongSelect(dong)}>
               {dong}
             </StyledButton>
           ))}
-
           {selectedSido && (
             <StyledButton onClick={() => {
               if (selectedGugun) {
                 setSelectedGugun(null);
-              } else if (selectedSido) {
-                setSelectedSido(null);
               } else {
-                setIsOpen(false);
+                setSelectedSido(null);
               }
             }}>이전</StyledButton>
           )}
